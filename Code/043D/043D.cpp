b07aab3e210742552d1a4aa40bc6a76bc146ef27
zhangyichengm <zhangyichengm@qq.com>
2024-03-06 23:30:58 +0800

    进入3D，让箱子有3D的感觉

---
+++ Code/043D/043D.cpp
@@ -0,0 +1,168 @@
+﻿#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include <iostream>
+#include <math.h>
+#include "Shader.h"
+#include "stb_image.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+using namespace std;
+using namespace glm;
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+void procInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    //顶点数据
+    float vertices[] = {
+        //     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+             0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+             0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
+    };
+
+    unsigned int indices[] = {
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+    };
+    //数据进入顶点缓存
+    unsigned int VBO, VAO, EBO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+    //链接顶点属性 坐标
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //颜色
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //纹理坐标
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+
+    glBindVertexArray(0);
+
+    Shader sh("Shader.vs", "Shader.fs");
+
+
+
+    unsigned int texture1;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    stbi_set_flip_vertically_on_load(true);
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+    mat4 model = mat4(1.0f);
+    model=rotate(model, radians(-55.0f),vec3(1.0f,0.0f,0.0f));
+    mat4 view = mat4(1.0f);
+    view = translate(view,vec3(0.0f,0.0f,-3.0f));
+
+    mat4 projection=mat4(1.0f);
+    projection = perspective(radians(45.0f), (float)(SCR_WIDTH / SCR_HEIGHT),0.1f,100.0f);
+   
+    sh.use();
+    sh.setInt("texture1", 0);
+    sh.setMat4("model", model);
+    sh.setMat4("view", view);
+    sh.setMat4("projection", projection);
+    while (!glfwWindowShouldClose(window))
+    {
+        procInput(window);
+
+        //render here
+        glClearColor(0.2f, 0.3f, 0.3f, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        sh.use();
+
+
+
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+
+        glBindVertexArray(VAO);
+      
+
+        mat4 trans = mat4(1.0f);
+        /*
+        trans = translate(trans, vec3(0.5f, 0, 0));
+        trans = rotate(trans, (float)glfwGetTime() * 0.1f, vec3(0, 0, 1.0f));
+        trans = scale(trans, vec3(0.5f, 0.5f, 0.5f));
+        unsigned int translateDoc = glGetUniformLocation(sh.ID, "transform");
+        glUniformMatrix4fv(translateDoc, 1, GL_FALSE, value_ptr(trans));*/
+
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+       
+        glBindVertexArray(0);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(sh.ID);
+    glfwTerminate();
+    return 0;
+}
+
